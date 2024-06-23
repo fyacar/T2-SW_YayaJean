039b6ccb3cc67077d0a118592838824395917a01
Jean Y <fyacar_14@hotmail.com>
2024-06-23 11:21:37 -0500

    pregunta01

---
+++ src/main/java/pe/edu/cibertec/T2_SW_YayaJean/T2SwYayaJeanApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.T2_SW_YayaJean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2SwYayaJeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2SwYayaJeanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/T2_SW_YayaJean/T2SwYayaJeanApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.T2_SW_YayaJean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2SwYayaJeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
